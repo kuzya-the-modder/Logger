8202bc41419a66be50b58c64edf1660581c21f66
Kuzya <kuzyathemodder@gmail.com>
2024-05-09 19:07:25 +0300

    Added Example

---
+++ Example/Program.cs
@@ -0,0 +1,11 @@
+﻿using Kuzya.Logs;
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.SetPath("D:\\Desktop");
+        var logger = new Logger();
+        logger.Log("Hello, World!");
+        logger.Dispose();
+    }
+}
